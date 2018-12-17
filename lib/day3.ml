c46fd6ef581dff980f750888113502efb0dfc54d
Chris Westra <cwestra@gmail.com>
2018-12-17 16:58:57 -0500

    Format using ocamlformat

--- lib/day3.ml
+++ lib/day3.ml
@@ -11,20 +11,22 @@ module Coordinate = struct
        let cmp_first = Int.compare x1 x2 in
        if cmp_first <> 0 then cmp_first
        else Int.compare y1 y2 *)
-
   end
 
   include T
-  include Comparator.Make(T)
+  include Comparator.Make (T)
 end
 
 module Claim = struct
   open Angstrom
+
   type t = {id: int; origin: Coordinate.t; size: int * int} [@@deriving sexp]
 
   let parser =
     let pint =
-      take_while1 (function '0' .. '9' -> true | _ -> false) >>| Int.of_string in
+      take_while1 (function '0' .. '9' -> true | _ -> false)
+      >>| Int.of_string
+    in
     let space = char ' ' in
     let tuple a b = (a, b) in
     let claimId = string "#" *> pint in
@@ -35,14 +37,13 @@ module Claim = struct
       (claimId <* space *> string "@" *> space)
       (origin <* space) size
 
-  let of_string s =
-    parse_string parser s |> Result.ok_or_failwith
+  let of_string s = parse_string parser s |> Result.ok_or_failwith
 
-  let to_sexp c =
-    sexp_of_t c
-  
+  let to_sexp c = sexp_of_t c
+
   (* a list of all coordinates taken up by the claim *)
   let claimed_coords c =
+    let width, height = c.size in
     [c.origin]
 end
 
@@ -53,13 +54,13 @@ let%expect_test _ =
   ((id 2)(coords(1 3))(size(4 4)))
   |}]
 
-let recordOccurence map (claim: Claim.t) =
+let recordOccurence map (claim : Claim.t) =
   match Map.find map claim.origin with
   | None ->
-    let empty_set = Set.empty (module Int) in
-    Map.set map ~key:claim.origin ~data: (Set.add empty_set claim.id)
+      let empty_set = Set.empty (module Int) in
+      Map.set map ~key:claim.origin ~data:(Set.add empty_set claim.id)
   | Some claim_ids ->
-    Map.set map ~key:claim.origin ~data:(Set.add claim_ids claim.id)
+      Map.set map ~key:claim.origin ~data:(Set.add claim_ids claim.id)
 
 let claimed_squares claim_strings =
   let coords = Map.empty (module Coordinate) in
@@ -68,13 +69,11 @@ let claimed_squares claim_strings =
 
 let multiple_claims claim_map =
   Map.filter claim_map ~f:(fun claims -> Set.length claims > 1)
-  |> Map.keys
-  |> List.length
+  |> Map.keys |> List.length
 
 let%expect_test _ =
-  let claims = ["#1 @ 1,3: 4x4"; "#2 @ 3,1: 4x4"; "#3 @ 5,5: 2x2";] in
+  let claims = ["#1 @ 1,3: 4x4"; "#2 @ 3,1: 4x4"; "#3 @ 5,5: 2x2"] in
   let multi_count = claimed_squares claims |> multiple_claims in
-  printf "%i\n" multi_count;
-  [%expect {|
+  printf "%i\n" multi_count ; [%expect {|
   4
   |}]
