f9ff0037b77fd31d26c9190f44d5395b30733f06
Chris Westra <cwestra@gmail.com>
2019-01-15 08:41:38 -0500

    Day 6 part 1 finished

--- bin/main.ml
+++ bin/main.ml
@@ -3,9 +3,11 @@ open Core
 
 let () =
   (* Day1.part1 () |> printf "Day 1 part 1: %i \n" |> ignore ; *)
-  printf "Running part 2 \n"
+  printf "Running day 6 \n";
 
 (* Day1.part2 () |> printf "Day 1 part 2: %i \n" |> ignore ;
   Day2.part1 () |> printf "Day 2 part 1: %i \n" |> ignore ;
   Day3.part1 () |> printf "Day 3 part 1: %i \n" |> ignore ;
   Day3.part2 () |> printf "Day 3 part 2: %s \n" |> ignore ; *)
+
+  Day6.part1 () |> printf "Day 6 part 1: %s \n" |> ignore ;
--- lib/day6.ml
+++ lib/day6.ml
@@ -8,17 +8,23 @@ let nested_fold outer inner accum f =
     ~f:(fun a' o -> List.fold ~init:a' ~f:(fun a'' i -> f a'' o i) inner)
     outer
 
-type distance =  char * int [@@deriving sexp]
+type distance = char * int [@@deriving sexp]
+
+type char_int_map = int Map.M(Char).t [@@deriving sexp]
 
 module Grid = struct
   type point_location = Interior | Exterior [@@deriving sexp]
 
-  type nearest = | Input of distance | Tie [@@deriving sexp]
-    
-  type point = {r: int; c: int; nearest: nearest; location: point_location} [@@deriving sexp]
+  type nearest = Input of distance | Tie [@@deriving sexp]
+
+  type point = {r: int; c: int; nearest: nearest; location: point_location}
+  [@@deriving sexp]
+
   type input_pt = {r: int; c: int; label: char} [@@deriving sexp]
 
-  type t = {inputs: input_pt list; bounds: (int * int * int * int); points: point list} [@@deriving sexp]
+  type t =
+    {inputs: input_pt list; bounds: int * int * int * int; points: point list}
+  [@@deriving sexp]
 
   let point_of_string i s =
     let coords = Str.split (Str.regexp ", ") s |> List.map ~f:Int.of_string in
@@ -28,25 +34,21 @@ module Grid = struct
 
   let list_to_points l = List.mapi ~f:point_of_string l
 
-  let get_distance (i: input_pt) r c = 
+  let get_distance (i : input_pt) r c =
     let dr = abs (i.r - r) in
     let dc = abs (i.c - c) in
     let out = dr + dc in
-    (* printf "From %c:(%i, %i) to (%i, %i) is %i, %i = %i \n" i.label i.r i.c r c dr dc out; *)
     out
 
-
   let nearest_input x y inputs =
-    let k = List.map inputs ~f:(fun j -> (j.label, get_distance j x y))
-    |> List.sort ~compare:(fun (_, d1) (_, d2) -> Int.compare d1 d2) in
-    (* List.sexp_of_t sexp_of_distance k |> Sexp.to_string_hum |> Stdio.print_endline; *)
+    let k =
+      List.map inputs ~f:(fun j -> (j.label, get_distance j x y))
+      |> List.sort ~compare:(fun (_, d1) (_, d2) -> Int.compare d1 d2)
+    in
     match k with
-    | (i1, d1) :: (_, d2) :: _ ->
-      if d1 = d2 then Tie else Input (i1, d1)
+    | (i1, d1) :: (_, d2) :: _ -> if d1 = d2 then Tie else Input (i1, d1)
     | hd :: _ -> Input hd
     | [] -> failwith "Empty list of inputs"
-    (* sexp_of_distance o |> Sexp.to_string |> Stdio.print_endline; *)
-
 
   let bounding_box inputs =
     let rs = List.map ~f:(fun k -> k.r) inputs in
@@ -58,43 +60,67 @@ module Grid = struct
 
   let make str =
     let inputs = list_to_points str in
-    let (r1, c1, r2, c2) = bounding_box inputs in
+    let r1, c1, r2, c2 = bounding_box inputs in
     let rs = List.range r1 (r2 + 1) in
     let cs = List.range c1 (c2 + 1) in
-    let pts = nested_fold rs cs [] (fun a r c -> 
-      let location = match (r, c) with
-      | (a, b) when a = r1 || a = r2 || b = c1 || b = c2 -> Exterior
-      | _ -> Interior in
-      let nearest = (nearest_input r c inputs) in
-      {r; c; nearest; location} :: a
-    ) in
+    let pts =
+      nested_fold rs cs [] (fun a r c ->
+          let location =
+            match (r, c) with
+            | a, b when a = r1 || a = r2 || b = c1 || b = c2 -> Exterior
+            | _ -> Interior
+          in
+          let nearest = nearest_input r c inputs in
+          {r; c; nearest; location} :: a )
+    in
     {inputs; bounds= (r1, c1, r2, c2); points= List.rev pts}
 
-  let to_string g =
-    let point_strings = List.map g.points ~f:(fun x -> 
-      match x.nearest with
-      | Input (c, 0) -> Printf.sprintf "%c" (Char.of_int_exn ((Char.to_int c) - 32))
-      | Input (c, _) -> Printf.sprintf "%c" c
-      | Tie ->  Printf.sprintf "."
-    ) in
-    let (_, c1, _, c2) = g.bounds in
-    let width = c2 - c1 + 1 in
-    let groups = List.groupi ~break:(fun i _ _ -> i % width = 0) point_strings in
-    List.map groups ~f:(fun x -> String.concat x)
-    |> String.concat ~sep:"\n"
+  let interior_groups g =
+    let group_type points =
+      match List.find points ~f:(fun p -> phys_equal p.location Exterior) with
+      | Some _ -> Exterior
+      | None -> Interior
+    in
+    List.filter_map g.points ~f:(fun point ->
+        match point.nearest with
+        | Tie -> None
+        | Input (label, _) -> Some (label, point) )
+    |> Map.of_alist_fold (module Char) ~init:[] ~f:(fun acc p -> p :: acc)
+    |> Map.filter ~f:(fun v ->
+           match group_type v with Interior -> true | Exterior -> false )
+    |> Map.map ~f:(fun v -> List.length v)
 
+  let to_string g =
+    let point_strings =
+      List.map g.points ~f:(fun x ->
+          match x.nearest with
+          | Input (c, 0) ->
+              Printf.sprintf "%c" (Char.of_int_exn (Char.to_int c - 32))
+          | Input (c, _) -> Printf.sprintf "%c" c
+          | Tie -> Printf.sprintf "." )
+    in
+    let _, c1, _, c2 = g.bounds in
+    let width = c2 - c1 + 1 in
+    let groups =
+      List.groupi ~break:(fun i _ _ -> i % width = 0) point_strings
+    in
+    List.map groups ~f:(fun x -> String.concat x) |> String.concat ~sep:"\n"
 end
 
-let read_file () = In_channel.read_lines "./inputs/4.txt"
+let read_file () = In_channel.read_lines "./inputs/6.txt"
+
+let part1 () =
+  let sample_input = read_file () in
+  (* let sample_input = ["1, 1"; "3, 3"] in *)
+  Grid.make sample_input |> Grid.interior_groups |> sexp_of_char_int_map
+  |> Sexp.to_string_hum
 
 let%expect_test _ =
   let sample_input = ["1, 1"; "1, 6"; "8, 3"; "3, 4"; "5, 5"; "8, 9"] in
   (* let sample_input = ["1, 1"; "3, 3"] in *)
-
-  Grid.make sample_input
-  |> Grid.to_string
-  |> Stdio.print_endline;
-  [%expect {|
+  Grid.make sample_input |> Grid.to_string |> Stdio.print_endline ;
+  [%expect
+    {|
 aaaaa.cccc
 aAaaa.cccc
 aaaddecccc
@@ -106,4 +132,13 @@ bbb.eeefff
 bbb.eeffff
 bbb.ffffFf
 bbb.ffffff
+|}]
+
+let%expect_test _ =
+  let sample_input = ["1, 1"; "1, 6"; "8, 3"; "3, 4"; "5, 5"; "8, 9"] in
+  (* let sample_input = ["1, 1"; "3, 3"] in *)
+  Grid.make sample_input |> Grid.interior_groups |> sexp_of_char_int_map
+  |> Sexp.to_string_hum |> Stdio.print_endline ;
+  [%expect {|
+    ((d 9) (e 17))
 |}]
