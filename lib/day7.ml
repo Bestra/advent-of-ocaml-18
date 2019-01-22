6c5c7875957a698aa842039cd55d8639dc7a7d4f
Chris Westra <cwestra@gmail.com>
2019-01-21 23:28:12 -0500

    WIP

--- lib/day7.ml
+++ lib/day7.ml
@@ -19,12 +19,12 @@ module Dag = struct
     let parent, child = e in
     let m1 =
       Map.update dag parent ~f:(function
-        | None -> {empty_node with outs= [child]}
-        | Some n -> {n with outs= child :: n.outs} )
+          | None -> {empty_node with outs= [child]}
+          | Some n -> {n with outs= child :: n.outs} )
     in
     Map.update m1 child ~f:(function
-      | None -> {empty_node with ins= [parent]}
-      | Some n -> {n with ins= parent :: n.ins} )
+        | None -> {empty_node with ins= [parent]}
+        | Some n -> {n with ins= parent :: n.ins} )
 
   let children dag node =
     let n = Map.find_exn dag node in
@@ -33,63 +33,6 @@ module Dag = struct
   let of_input_list s =
     s |> List.map ~f:edge_of_string
     |> List.fold ~init:(Map.empty (module Char)) ~f:add_edge
-
-  module Degrees = struct
-    type list_t = char * int [@@deriving sexp]
-
-    type t = int Map.M(Char).t [@@deriving sexp]
-
-    let compare (n1, d1) (n2, d2) =
-      match Int.compare d1 d2 with 0 -> Char.compare n1 n2 | l -> l
-
-    let of_dag dag =
-      let in_degree dag n =
-        match Map.find dag n with Some i -> List.length i.ins | None -> 0
-      in
-      Map.keys dag
-      |> List.map ~f:(fun k -> (k, in_degree dag k))
-      |> List.sort ~compare
-      |> Map.of_alist_exn (module Char)
-
-    let sorted_list t = Map.to_alist t |> List.sort ~compare
-
-    let zero_in_nodes t =
-      t |> sorted_list
-      |> List.filter_map ~f:(fun (c, i) ->
-             match i with 0 -> Some c | _ -> None )
-  end
-
-  let topo_sort (edges : t) =
-    let decrement_children node degree_map =
-      let kids = children edges node in
-      (* let k = (List.sexp_of_t sexp_of_char kids) |> Sexp.to_string_hum in
-      printf "Children of %c are %s\n" node k; *)
-      List.fold kids ~init:(degree_map, []) ~f:(fun (m, zeros) kid ->
-          let old_val = Map.find_exn m kid in
-          let new_val = old_val - 1 in
-          let new_zeros =
-            match new_val with 0 -> List.append zeros [kid] | _ -> zeros
-          in
-          (Map.set m ~key:kid ~data:new_val, new_zeros) )
-    in
-    let rec go output degree_map zero_queue =
-      (* let o = (List.sexp_of_t sexp_of_char output) |> Sexp.to_string_hum in
-      let d = (Degrees.sexp_of_t degree_map) |> Sexp.to_string_hum ~indent:1 in
-      let z = (List.sexp_of_t sexp_of_char zero_queue) |> Sexp.to_string_hum in
-      printf "output: %s degrees: %s queue: %s \n" o d z; *)
-      match zero_queue with
-      | [] -> output
-      | hd :: tl ->
-          let new_map, new_zeros = decrement_children hd degree_map in
-          go
-            (List.append output [hd])
-            new_map
-            (List.append tl new_zeros |> List.sort ~compare:Char.compare)
-    in
-    let degrees = Degrees.of_dag edges in
-    go [] degrees (Degrees.zero_in_nodes degrees)
-
-  (* subtract one from all the degrees of hd's children.  if any of them are now zero append them to the queue *)
 
   let dot_of_edges (t : t) accessor =
     let node_to_string i o =
@@ -109,10 +52,137 @@ module Dag = struct
   let dot_of_parents (t : t) = dot_of_edges t ins
 end
 
+module StatefulGraph = struct
+  type list_t = char * int [@@deriving sexp]
+
+  type in_degrees = int Map.M(Char).t [@@deriving sexp]
+
+  type state = {edges: Dag.t; completed: char list; in_degrees: in_degrees}
+
+  let compare (n1, d1) (n2, d2) =
+    match Int.compare d1 d2 with 0 -> Char.compare n1 n2 | l -> l
+
+  let of_dag (dag : Dag.t) =
+    let in_degree n =
+      match Map.find dag n with Some i -> List.length i.ins | None -> 0
+    in
+    let in_degrees =
+      Map.keys dag
+      |> List.map ~f:(fun k -> (k, in_degree k))
+      |> List.sort ~compare
+      |> Map.of_alist_exn (module Char)
+    in
+    {edges= dag; completed= []; in_degrees}
+
+  let zero_degree_nodes in_deg =
+    in_deg |> Map.to_alist |> List.sort ~compare
+    |> List.filter_map ~f:(fun (c, i) -> match i with 0 -> Some c | _ -> None)
+
+  let complete state node =
+    let kids = Dag.children state.edges node in
+    (* let k = (List.sexp_of_t sexp_of_char kids) |> Sexp.to_string_hum in
+        printf "Children of %c are %s\n" node k; *)
+    List.fold kids ~init:state ~f:(fun {in_degrees; completed; _} kid ->
+        let old_val = Map.find_exn in_degrees kid in
+        let new_val = old_val - 1 in
+        let new_completed =
+          match new_val with
+          | 0 -> List.append completed [kid]
+          | _ -> completed
+        in
+        { state with
+          in_degrees= Map.set in_degrees ~key:kid ~data:new_val
+        ; completed= new_completed } )
+
+  let to_complete state =
+    let char_set = Set.of_list (module Char) state.completed in
+    List.filter ~f:(fun x -> Set.mem char_set x |> not) (zero_degree_nodes state.in_degrees)
+
+  let topo_sort t =
+    let decrement_children node degree_map =
+      let kids = Dag.children t.edges node in
+      (* let k = (List.sexp_of_t sexp_of_char kids) |> Sexp.to_string_hum in
+         printf "Children of %c are %s\n" node k; *)
+      List.fold kids ~init:(degree_map, []) ~f:(fun (m, zeros) kid ->
+          let old_val = Map.find_exn m kid in
+          let new_val = old_val - 1 in
+          let new_zeros =
+            match new_val with 0 -> List.append zeros [kid] | _ -> zeros
+          in
+          (Map.set m ~key:kid ~data:new_val, new_zeros) )
+    in
+    let rec go output degree_map zero_queue =
+      (* let o = (List.sexp_of_t sexp_of_char output) |> Sexp.to_string_hum in
+         let d = (StatefulGraph.sexp_of_t degree_map) |> Sexp.to_string_hum ~indent:1 in
+         let z = (List.sexp_of_t sexp_of_char zero_queue) |> Sexp.to_string_hum in
+         printf "output: %s StatefulGraph: %s queue: %s \n" o d z; *)
+      match zero_queue with
+      | [] -> output
+      | hd :: tl ->
+        let new_map, new_zeros = decrement_children hd degree_map in
+        go
+          (List.append output [hd])
+          new_map
+          (List.append tl new_zeros |> List.sort ~compare:Char.compare)
+    in
+    go [] t.in_degrees (zero_degree_nodes t.in_degrees)
+end
+
+module Scheduler = struct
+  type worker = Working of {node: char; done_at: int} | Free
+  [@@deriving sexp]
+
+  let run_graph worker_count a_time sorted_nodes =
+    let finish_jobs current_time workers out_nodes =
+      List.fold workers ~init:([], out_nodes) ~f:(fun (workers, nodes) w ->
+          match w with
+          | Free -> (w :: workers, nodes)
+          | Working {node; done_at} ->
+            if current_time >= done_at then (Free :: workers, node :: nodes)
+            else (w :: workers, nodes) )
+    in
+    let start_jobs current_time workers in_nodes =
+      let duration n = Char.to_int n - 65 + a_time in
+      List.fold workers ~init:([], in_nodes) ~f:(fun (workers, nodes) w ->
+          match w with
+          | Working _ -> (w :: workers, nodes)
+          | Free -> (
+              match nodes with
+              | [] -> (w :: workers, nodes)
+              | hd :: tl ->
+                ( Working {node= hd; done_at= duration hd + current_time}
+                  :: workers
+                , tl ) ) )
+    in
+    let rec advance second workers input_nodes finished_nodes =
+      let p_char_list l = List.sexp_of_t sexp_of_char l |> Sexp.to_string in
+      printf "%i %s %s %s \n" second
+        (List.sexp_of_t sexp_of_worker workers |> Sexp.to_string)
+        (p_char_list input_nodes)
+        (p_char_list finished_nodes) ;
+      match input_nodes with
+      | [] -> (second, finished_nodes)
+      | nodes ->
+        (* first finish existing jobs, push those nodes to out  *)
+        let updated_workers, newly_finished =
+          finish_jobs second workers finished_nodes
+        in
+        (* then pull off nodes from in to give to free workers*)
+        let final_workers, consumed_inputs =
+          start_jobs second updated_workers nodes
+        in
+        advance (second + 1) final_workers consumed_inputs newly_finished
+    in
+    advance 0
+      (List.range 0 worker_count |> List.map ~f:(fun _ -> Free))
+      sorted_nodes []
+end
+
 let read_file () = In_channel.read_lines "./inputs/7.txt"
 
 let part1 () =
-  read_file () |> Dag.of_input_list |> Dag.topo_sort |> String.of_char_list
+  read_file () |> Dag.of_input_list |> StatefulGraph.of_dag
+  |> StatefulGraph.topo_sort |> String.of_char_list
 
 let part2 () = "bar"
 
@@ -154,17 +224,14 @@ let%expect_test _ =
 |}]
 
 let%expect_test _ =
-  sample_input |> Dag.of_input_list |> Dag.Degrees.of_dag
-  |> Dag.Degrees.sorted_list
-  |> List.sexp_of_t Dag.Degrees.sexp_of_list_t
-  |> Sexp.to_string_hum |> Stdio.print_endline ;
+  sample_input |> Dag.of_input_list |> StatefulGraph.of_dag
+  |> StatefulGraph.topo_sort |> String.of_char_list |> Stdio.print_endline ;
   [%expect {|
-  ((C 0) (A 1) (B 1) (D 1) (F 1) (E 3))
-|}]
+   CABDFE
+   |}]
 
 let%expect_test _ =
-  sample_input |> Dag.of_input_list |> Dag.topo_sort |> String.of_char_list
-  |> Stdio.print_endline ;
+  ['C'; 'A'; 'B'; 'D'; 'F'; 'E'] |> Scheduler.run_graph 2 1 |> ignore ;
   [%expect {|
    CABDFE
    |}]
